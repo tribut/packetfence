ef8ab186718c1ec7a6466fbce010c7dc43abc52d
Durand Fabrice <fdurand@inverse.ca>
2013-09-25 15:11:24 -0400

    Fix syntax

--- lib/pf/web.pm
+++ lib/pf/web.pm
@@ -262,9 +262,9 @@ sub generate_windowsconfig_provisioning_page {
     render_template($portalSession, 'release_with_execonfig.html');
 }
 
-=item supports_mobileconfig_provisioning
+=item supports_androidconfig_provisioning
 
-Validating that the node supports mobile configuration provisioning, that it's configured
+Validating that the node supports android configuration provisioning, that it's configured
 and that the node's category matches the configuration.
 
 =cut
@@ -293,7 +293,7 @@ sub supports_androidconfig_provisioning {
     return $FALSE;
 }
 
-=item generate_mobileconfig_provisioning_page
+=item generate_androidconfig_provisioning_page
 
 Offers a page that links to the proper provisioning XML.
 
@@ -863,10 +863,6 @@ sub end_portal_session {
     }
     if (pf::web::supports_windowsconfig_provisioning($portalSession)) {
         pf::web::generate_windowsconfig_provisioning_page($portalSession);
-        exit(0);
-    }
-    if (pf::web::supports_androidconfig_provisioning($portalSession)) {
-        pf::web::generate_androidconfig_provisioning_page($portalSession);
         exit(0);
     }
     if (pf::web::supports_androidconfig_provisioning($portalSession)) {
