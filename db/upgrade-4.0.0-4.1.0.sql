5bb95ea58ebbcfd43f7f36510fdfacdbcbbac0ce
Durand Fabrice <fdurand@inverse.ca>
2013-11-05 09:54:44 -0500

    Update mysql scheme

--- db/upgrade-4.0.0-4.1.0.sql
+++ db/upgrade-4.0.0-4.1.0.sql
@@ -3,3 +3,13 @@
 --
 
 ALTER TABLE `temporary_password` MODIFY category int DEFAULT NULL;
+
+--
+-- Alter for dynamic controller
+--
+
+ALTER TABLE locationlog 
+    ADD `switch_ip` varchar(17) DEFAULT NULL,
+    ADD `switch_mac` varchar(17) DEFAULT NULL;
+
+UPDATE locationlog SET switch_ip = switch;
