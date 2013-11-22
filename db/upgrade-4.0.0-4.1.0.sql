cecba6d4be9ba3293c0e1cb2aa205e7fe0f82afd
Francis Lachapelle <flachapelle@inverse.ca>
2013-11-22 16:19:56 -0500

    Add management of new bandwidth balance for nodes

--- db/upgrade-4.0.0-4.1.0.sql
+++ db/upgrade-4.0.0-4.1.0.sql
@@ -3,3 +3,8 @@
 --
 
 ALTER TABLE `temporary_password` MODIFY category int DEFAULT NULL;
+
+---
+--- Add a column to store the remaining available bandwidth usage of a node
+---
+ALTER TABLE node ADD `bandwidth_balance` int unsigned AFTER `time_balance`;
