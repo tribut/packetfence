f60f3cfb807b21c55ee5319a1fb4bc7bd4d0a6a6
Francis Lachapelle <flachapelle@inverse.ca>
2013-11-26 13:36:24 -0500

    Rename column 'timeleft' to 'time_balance'
    The violations related to 'time limits' are now opened using the proper
    violation trigger instead of using a hardcoded violation ID.

--- db/upgrade-4.0.0-4.1.0.sql
+++ db/upgrade-4.0.0-4.1.0.sql
@@ -5,6 +5,11 @@
 ALTER TABLE `temporary_password` MODIFY category int DEFAULT NULL;
 
 ---
---- Add a column to store the remaining available bandwidth usage of a node
+--- Add a column to store the time balance of a node
+---
+ALTER TABLE node ADD `time_balance` int unsigned AFTER `lastskip`;
+
+---
+--- Add a column to store the bandwidth balance of a node
 ---
 ALTER TABLE node ADD `bandwidth_balance` int unsigned AFTER `time_balance`;
